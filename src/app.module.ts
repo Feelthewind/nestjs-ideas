7a8092f7f0cd82756fb9fa4620867c0f0f6e97fa
lcs <skyisthelimitless@gmail.com>
2019-09-12 02:08:08 +0900

    Added initial idea module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IdeaModule } from './idea/idea.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), IdeaModule],
   controllers: [AppController],
   providers: [AppService],
 })
