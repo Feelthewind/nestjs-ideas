f24507f822fe8275e7f08f8d0222811297117efc
lcs <skyisthelimitless@gmail.com>
2019-09-12 02:00:03 +0900

    Set up database and environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from "@nestjs/common";
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap')
 }
 bootstrap();
